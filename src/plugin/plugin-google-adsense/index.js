4cee1b4b16a6ee40f1e7d62f5636c85919814452
YancyQi2002 <xiaoyixiu2002@qq.com>
2021-01-01 18:42:37 +0800

    initial commit

--- src/plugin/plugin-google-adsense/index.js
+++ src/plugin/plugin-google-adsense/index.js
@@ -7,7 +7,9 @@ module.exports = function(context, options) {
           {
             tagName: "script",
             attributes: {
+              "rel": "preload",
               "data-ad-client": "ca-pub-2500903599637609",
+              defer: true,
               async: true,
               src:
                 "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
